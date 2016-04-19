a84ea71ef4256274dd451104111a0dc6d2b33575
Mathias Karstädt <mathias.karstaedt@gmail.com>
2016-04-19 15:10:43 +0200

    test: added initial test
    add test using mocha and chai

---
+++ src/index.test.js
@@ -0,0 +1,10 @@
+var expect = require('chai').expect
+var time_helpers = require('./index')
+
+describe('time-helpers', function () {
+
+  it('should be an object', function () {
+    expect(time_helpers).to.be.a('object')
+  })
+  
+})
