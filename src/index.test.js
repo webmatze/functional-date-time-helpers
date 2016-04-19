5d1067c97b213836aba5b67ab0d3967b9f4b42ba
Mathias Karstädt <mathias.karstaedt@gmail.com>
2016-04-19 16:25:17 +0200

    test(startOfDay): added test

--- src/index.test.js
+++ src/index.test.js
@@ -6,5 +6,36 @@ describe('time-helpers', function () {
   it('should be an object', function () {
     expect(time_helpers).to.be.a('object')
   })
-  
+
+  describe('startOfDay', function () {
+
+    var date = new Date()
+
+    it('should be a function', function () {
+      expect(time_helpers.startOfDay).to.be.a('function')
+    })
+
+    it('should take one parameter', function () {
+      expect(time_helpers.startOfDay).to.have.length(1)
+    })
+
+    it('should return a Date object', function () {
+      expect(time_helpers.startOfDay(date)).to.be.an.instanceof(Date)
+    })
+
+    it('should return a Date with same day, month, year', function () {
+      expect(time_helpers.startOfDay(date).getDate()).to.equal(date.getDate())
+      expect(time_helpers.startOfDay(date).getMonth()).to.equal(date.getMonth())
+      expect(time_helpers.startOfDay(date).getFullYear()).to.equal(date.getFullYear())
+    })
+
+    it('should return a Date with hours, minutes, seconds and milliseconds set to 0', function () {
+      expect(time_helpers.startOfDay(date).getHours()).to.equal(0)
+      expect(time_helpers.startOfDay(date).getMinutes()).to.equal(0)
+      expect(time_helpers.startOfDay(date).getSeconds()).to.equal(0)
+      expect(time_helpers.startOfDay(date).getMilliseconds()).to.equal(0)
+    })
+
+  })
+
 })
