94097c9a128eae66fa7996b06329fd6bb7acbb62
Mathias Karstädt <mathias.karstaedt@gmail.com>
2016-04-22 01:10:28 +0200

    fix(performance): use current version of moment.js and add benchmarks

--- perf/perf.js
+++ perf/perf.js
@@ -1,27 +1,47 @@
 var Benchmark = require('benchmark')
 
-var moment = require('momentjs')
+var moment = require('moment')
 var fdth = require('../src/index')
 
 var suites = []
 
 suites.push(
-  new Benchmark.Suite('Compare fdth.startOfDay with moment.startOf("day")').add('fdth.startOfDay', function () {
+  new Benchmark.Suite('Compare fdth.startOfDay with moment#startOf').add('fdth.startOfDay', function () {
     var today = new Date()
     fdth.startOfDay(today)
   })
-    .add("moment().startOf('day')", function () {
+    .add("moment#startOf", function () {
       moment().startOf('day')
     })
 )
 
 suites.push(
-  new Benchmark.Suite('Compare fdth.addTime with moment.add').add('fdth.addTime', function () {
+  new Benchmark.Suite('Compare fdth.endOfDay with moment#endOf').add('fdth.endOfDay', function () {
+    var today = new Date()
+    fdth.endOfDay(today)
+  })
+    .add("moment#endOf", function () {
+      moment().endOf('day')
+    })
+)
+
+suites.push(
+  new Benchmark.Suite('Compare fdth.addTime with moment#add').add('fdth.addTime', function () {
     var today = new Date()
     fdth.addTime(fdth.days(3), today)
   })
-    .add("moment().startOf('day')", function () {
+    .add("moment#add", function () {
       moment().add(3, 'days')
+    })
+)
+
+suites.push(
+  new Benchmark.Suite('Compare fdth.subTime with moment#subtract').add('fdth.subTime', function () {
+    var today = new Date()
+    fdth.subTime(fdth.days(3), today)
+  })
+    .add("moment#subtract", function () {
+      moment().subtract(3, 'days')
     })
 )
 
